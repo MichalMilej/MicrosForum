8d829370870868c23c1e7c8aaba21a379ada31ac
Michał Milej <michal.45454@gmail.com>
2024-04-02 20:59:29 +0200

    PostMicros project created

---
+++ PostsMicros/src/main/java/pl/milejmichal/postsmicros/PostsMicrosApplication.java
@@ -0,0 +1,13 @@
+package pl.milejmichal.postsmicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsMicrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsMicrosApplication.class, args);
+	}
+
+}
---
+++ PostsMicros/src/test/java/pl/milejmichal/postsmicros/PostsMicrosApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milejmichal.postsmicros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsMicrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
