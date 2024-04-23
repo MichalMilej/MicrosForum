616ad15aa2fb30e13e790ffec23cfa138ea80571
Michał Milej <michal.45454@gmail.com>
2024-04-23 19:29:04 +0200

    NotifMicros created|

---
+++ pl.milejmichal.notifmicros/src/main/java/pl/michalmilej/notifmicros/NotifMicrosApplication.java
@@ -0,0 +1,13 @@
+package pl.michalmilej.notifmicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifMicrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifMicrosApplication.class, args);
+    }
+
+}
